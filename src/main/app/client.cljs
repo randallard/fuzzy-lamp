dfa12d138db0e1d12011d9cd1f5a76e5a28a23df
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-11-04 14:09:00 -0700

    board set with start space marked

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -6,15 +6,79 @@
     [com.fulcrologic.fulcro.dom :as dom]
     [com.fulcrologic.fulcro.algorithms.merge :as merge]
     [com.fulcrologic.fulcro.algorithms.data-targeting :as targeting]))
-
-(defsc Space [this {:space/keys [id row column blocked occupied]}]
+(def new-board {:board/id 1
+                :board/size 3
+                :board/rows [{:row/id 4
+                              :row/number 4
+                              :row/type :row-type/goal}
+                             {:row/id 3
+                              :row/number 3
+                              :row/type :row-type/spaces
+                              :row/spaces [{:space/id 7
+                                            :space/number 1
+                                            :space/status :free
+                                            :space/occupied nil}
+                                           {:space/id 8
+                                            :space/number 2
+                                            :space/status :free
+                                            :space/occupied nil}
+                                           {:space/id 9
+                                            :space/number 3
+                                            :space/status :free
+                                            :space/occupied nil}]}
+                             {:row/id 2
+                              :row/number 2
+                              :row/type :row-type/spaces
+                              :row/spaces [{:space/id 4
+                                            :space/number 1
+                                            :space/status :free
+                                            :space/occupied nil}
+                                           {:space/id 5
+                                            :space/number 2
+                                            :space/status :free
+                                            :space/occupied nil}
+                                           {:space/id 6
+                                            :space/number 3
+                                            :space/status :free
+                                            :space/occupied nil}]}
+                             {:row/id 1
+                              :row/number 1
+                              :row/type :row-type/spaces
+                              :row/spaces [{:space/id 1
+                                            :space/number 1
+                                            :space/status :free
+                                            :space/occupied nil}
+                                           {:space/id 2
+                                            :space/number 2
+                                            :space/status :free
+                                            :space/occupied [{:occupied/player :us
+                                                              :occupied/steps [1]}]}
+                                           {:space/id 3
+                                            :space/number 3
+                                            :space/status :free
+                                            :space/occupied nil}]}]})
+(defn space-css [type occupied]
+  {:className (str "space "
+                (if (not (nil? occupied)) "occupied ")
+                   (if (= type :row-type/goal) "goal "))})
+(defsc Space [this {:space/keys [id number status occupied]}]
   {}
-  (dom/div "Space[" id "] (" column "," row ")"))
+  (dom/span (space-css nil occupied) "Space[" number "]"))
 (def ui-space (comp/factory Space {:keyfn :space/id}))
-(defsc Board [this {:board/keys [id size] :as props}]
-  {:query [:board/id :board/size]
-   :ident :board/id}
-  (dom/div (dom/h2 "Board [" id "] Size " size)))
+
+(defsc Row [this {:row/keys [id number type spaces] :as props}]
+  {}
+  (dom/div (str "Row[" number "] type " type)
+           (dom/div {:style {:padding "15px"}}
+                    (cond
+                      (= type :row-type/goal) (dom/span (space-css type nil) "GOAL")
+                      (= type :row-type/spaces) (map ui-space spaces)))))
+(def ui-row (comp/factory Row {:keyfn :row/id}))
+
+(defsc Board [this {:board/keys [id size rows] :as props}]
+  {}
+  (dom/div (dom/h2 "Board [" id "] Size " size)
+           (dom/div (map ui-row rows))))
 
 (def ui-board (comp/factory Board {:keyfn :board/id}))
 
@@ -41,11 +105,7 @@
 
 (comment
   (app/current-state app)
-  (merge/merge-component! app Board {:board/id 1
-                                     :board/size 3
-                                     })
+  (merge/merge-component! app Board new-board)
   (app/schedule-render! app)
-  (reset! (::app/state-atom app) {:root {:board/id 1
-                                         :board/size 3
-                                         }})
+  (reset! (::app/state-atom app) {:root new-board})
   )
