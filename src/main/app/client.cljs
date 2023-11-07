60d64b47cea0ba15ffd0e97fd95525cd5a4be354
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-11-06 22:31:53 -0800

    initial state has button to select size of 2, click sets board of size 2

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -69,7 +69,9 @@
 (def ui-plan (comp/factory Plan {:keyfn :plan/id}))
 (defsc Board [this {:board/keys [id size rows plans] :as props}]
   {:query [:board/id :board/size {:board/rows (comp/get-query Row)} {:board/plans (comp/get-query Plan)}]
-   :ident :board/id}
+   :ident :board/id
+   :initial-state {:board/id :param/id
+                   :board/size :param/size}}
   (dom/div {:style {:width "100%"}}
     (dom/div {:style {:float "left" :padding "10px"}}
            (dom/h2 {} "Board [" id "] Size " size)
@@ -80,12 +82,44 @@
 
 (def ui-board (comp/factory Board {:keyfn :board/id}))
 
+(def board-size-2 [{:row/id 2
+                    :row/number 2
+                    :row/type :row-type/spaces
+                    :row/spaces [{:space/id 3
+                                  :space/number 1
+                                  :space/status :free
+                                  :space/occupied []}
+                                 {:space/id 4
+                                  :space/number 2
+                                  :space/status :free
+                                  :space/occupied []}]}
+                   {:row/id 1
+                    :row/number 1
+                    :row/type :row-type/spaces
+                    :row/spaces [{:space/id 1
+                                  :space/number 1
+                                  :space/status :free
+                                  :space/occupied []}
+                                 {:space/id 2
+                                  :space/number 2
+                                  :space/status :free
+                                  :space/occupied []}]}])
+(defmutation get-board-of-size [{:board/keys [id]}]
+  (action [{:keys [state]}]
+          (swap! state assoc-in [:board/id 1 :board/size] 2)
+          (swap! state assoc-in [:board/id id :board/rows] board-size-2)))
 (defonce app (-> (app/fulcro-app) (with-react18)))
 
 (defsc Root [this {:root/keys [board]}]
   {:query [{:root/board (comp/get-query Board)}]
-   :initial-state {:root/board {:id 1 :size 3}}}
-  (dom/div {} (ui-board board)))
+   :initial-state {:root/board {:id 1
+                                :size 0}}}
+  (cond (zero? (:board/size board))
+        (dom/div {} "select board size: "
+                 (dom/button {:onClick #(comp/transact!
+                                          this [(get-board-of-size {:board/id (:board/id board)})])
+                              :style {:margin "0px 15px"}}  " 2 "))
+        :else (dom/div {} (ui-board board))))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
@@ -104,5 +138,11 @@
 
 (comment
   (swap! (::app/state-atom app) assoc-in [:occupied/id 1 :occupied/steps] [1 2])
+  (swap! (::app/state-atom app) assoc-in [:board/id 1 :board/size] 1)
+  (swap! (::app/state-atom app) update-in [:board/id 1 :board/size] inc)
+  (comp/transact! app [(get-board-of-size {:board/id 1})])
   (app/current-state app)
+  (merge/merge-component! app Board {:board/id 1
+                                     :board/size 2}
+                          :replace [:root/board])
   )
