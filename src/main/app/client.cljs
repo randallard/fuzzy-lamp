c061786b6aef095889254bc2d0c021f9210c98a9
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-11-06 16:58:39 -0800

    added options for each dom element

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -25,7 +25,7 @@
    :initial-state {:occupied/id :param/id
                    :occupied/player :param/player
                    :occupied/steps :param/steps}}
-  (dom/span (str "By " player " at " steps)))
+  (dom/span {} (str "By " player " at " steps)))
 (def ui-occupied (comp/factory Occupied {:keyfn :occupied/id}))
 (defsc Space [this {:space/keys [id number status occupied]}]
   {:query [:space/id :space/number :space/status {:space/occupied (comp/get-query Occupied)}]
@@ -92,7 +92,7 @@
                                  :row/spaces [(comp/get-initial-state Space {:id 10
                                                                              :number 1
                                                                              :status :free})]}))}
-  (dom/div #_(str "Row[" number "] type " type)
+  (dom/div {} #_(str "Row[" number "] type " type)
            (dom/div {:style {:padding "5px"}}
                     (cond
                       (= type :row-type/goal) (dom/span (space-css type nil) "GOAL")
@@ -102,26 +102,26 @@
 (defsc Position [this {:position/keys [id row-number space-number]}]
   {:query [:position/id :position/row-number :position/space-number]
    :ident :position/id}
-  (dom/span "player at ( row " row-number ", column " space-number " )"))
+  (dom/span {} "player at ( row " row-number ", column " space-number " )"))
 (def ui-position (comp/factory Position {:keyfn :position/id}))
 (defsc Block [this {:block/keys [id row-number space-number]}]
   {:query [:block/id :block/row-number :block/space-number]
    :ident :block/id}
-  (dom/span "block set at ( row " row-number ", column " space-number " )"))
+  (dom/span {} "block set at ( row " row-number ", column " space-number " )"))
 (def ui-block (comp/factory Block {:keyfn :block/id}))
 (defsc Step [this {:step/keys [id number positions blocks]}]
   {:query [:step/id :step/number {:step/positions (comp/get-query Position)}
            {:step/blocks (comp/get-query Block)}]
    :ident :step/id}
-  (dom/div "Step " number " "
+  (dom/div {} "Step " number " "
            (map ui-position positions)
            (map ui-block blocks)))
 (def ui-step (comp/factory Step {:keyfn :step/id}))
 (defsc Plan [this {:plan/keys [id number steps] :as props}]
   {:query [:plan/id :plan/number {:plan/steps (comp/get-query Step)}]
    :ident :plan/id}
-  (dom/div (dom/p "Plan Number " number
-                  (dom/ul (map ui-step steps)))))
+  (dom/div {} (dom/p {} "Plan Number " number
+                  (dom/ul {} (map ui-step steps)))))
 (def ui-plan (comp/factory Plan {:keyfn :plan/id}))
 (defsc Board [this {:board/keys [id size rows plans] :as props}]
   {:query [:board/id :board/size {:board/rows (comp/get-query Row)} {:board/plans (comp/get-query Plan)}]
@@ -143,11 +143,11 @@
                    :board/plans []}}
   (dom/div {:style {:width "100%"}}
     (dom/div {:style {:float "left" :padding "10px"}}
-           (dom/h2 "Board [" id "] Size " size)
-           (dom/div (map ui-row rows)))
+           (dom/h2 {} "Board [" id "] Size " size)
+           (dom/div {} (map ui-row rows)))
     (dom/div {:style {:float "left" :padding "10px"}}
-           (dom/h2 "Plans")
-           (dom/div (map ui-plan plans)))))
+           (dom/h2 {} "Plans")
+           (dom/div {} (map ui-plan plans)))))
 
 (def ui-board (comp/factory Board {:keyfn :board/id}))
 
@@ -156,7 +156,7 @@
 (defsc Root [this {:root/keys [board]}]
   {:query [{:root/board (comp/get-query Board)}]
    :initial-state {:root/board {:id 1 :size 3}}}
-  (dom/div (ui-board board)))
+  (dom/div {} (ui-board board)))
 
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
