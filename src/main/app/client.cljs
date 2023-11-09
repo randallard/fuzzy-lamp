1ec3ad6c1de9997855b0bf76526bcbc2812262b6
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-11-09 12:59:03 -0800

    fix from before lost normalization

--- src/main/app/client.cljs
+++ src/main/app/client.cljs
@@ -19,14 +19,14 @@
                    (if (= space-status :occupied) "occupied ")
                    (if (= space-status :blocked) "blocked ")
                    (if (= type :row-type/goal) "goal "))})
-(defsc OccupiedSteps [this {:occupied/keys [id player steps]}]
-  {:query [:occupied/id :occupied/player :occupied/steps]
-   :ident :occupied/id
-   :initial-state {:occupied/id :param/id
-                   :occupied/player :param/player
-                   :occupied/steps :param/steps}}
+(defsc OccupiedSteps [this {:occupied-step/keys [id player steps]}]
+  {:query [:occupied-step/id :occupied-step/player :occupied-step/steps]
+   :ident :occupied-step/id
+   :initial-state {:occupied-step/id :param/id
+                   :occupied-step/player :param/player
+                   :occupied-step/steps :param/steps}}
   (dom/span {} (str "By " player " at " steps)))
-(def ui-occupied-steps (comp/factory OccupiedSteps {:keyfn :occupied/id}))
+(def ui-occupied-steps (comp/factory OccupiedSteps {:keyfn :occupied-step/id}))
 (defsc Space [this {:space/keys [id number status occupied-steps]}]
   {:query [:space/id :space/number :space/status {:space/occupied-steps (comp/get-query OccupiedSteps)}]
    :ident :space/id
@@ -54,7 +54,7 @@
                     {:row/id id
                      :row/number number
                      :row/type type
-                     :row/spaces [(comp/get-initial-state Space {:id (+ (* (- number 1) 3) 1)
+                     :row/spaces (cond (> id 3) [(comp/get-initial-state Space {:id (+ (* (- number 1) 3) 1)
                                                                  :number 1
                                                                  :status :free})
                                   (comp/get-initial-state Space {:id (+ (* (- number 1) 3) 2)
@@ -63,7 +63,14 @@
                                                                                :else :free)})
                                   (comp/get-initial-state Space {:id (+ (* (- number 1) 3) 3)
                                                                  :number 3
-                                                                 :status :free})]})}
+                                                                 :status :free})]
+                                       :else [(comp/get-initial-state Space {:id (+ (* (- number 1) 3) 1)
+                                                                             :number 1
+                                                                             :status :free})
+                                              (comp/get-initial-state Space {:id (+ (* (- number 1) 3) 2)
+                                                                             :number 2
+                                                                             :status (cond (= number 1) :occupied
+                                                                                           :else :free)})])})}
   (dom/div {} (dom/div {:style {:padding "5px"}} (map ui-space spaces))))
 
 (def ui-row (comp/factory Row {:keyfn :row/id}))
@@ -77,18 +84,25 @@
    :ident :block/id}
   (dom/span {} "block set at ( row " row-number ", column " space-number " )"))
 (def ui-block (comp/factory Block {:keyfn :block/id}))
-(defsc Step [this {:step/keys [id number positions blocks]}]
-  {:query [:step/id :step/number {:step/positions (comp/get-query Position)}
-           {:step/blocks (comp/get-query Block)}]
-   :ident :step/id}
-  (dom/div {} "Step " number " "
-           (map ui-position positions)
-           (map ui-block blocks)))
+(defsc Step [this {:step/keys [id number position-space-id block-space-id]}]
+  {:query [:step/id :step/number :step/position-space-id :step/block-space-id]
+   :ident :step/id
+   :initial-state {:step/id :param/id
+                   :step/number :param/number
+                   :step/position-space-id :param/position-space-id}}
+  (dom/div {} "Step " number " player at space id " position-space-id ))
 (def ui-step (comp/factory Step {:keyfn :step/id}))
 (defsc Plan [this {:plan/keys [id number steps] :as props}]
-  {:query [:plan/id :plan/number {:plan/steps (comp/get-query Step)}]
-   :ident :plan/id}
-  (dom/div {} (dom/p {} "Plan Number " number
+  {:query         [:plan/id :plan/number {:plan/steps (comp/get-query Step)}]
+   :ident         :plan/id
+   :initial-state {:plan/id     :param/id
+                   :plan/number :param/number
+                   :plan/steps  [{:id 1
+                                  :number 1
+                                  :position-space-id 2}]
+                   }
+   }
+  (dom/div {} (dom/div {} "Plan Number " number
                   (dom/ul {} (map ui-step steps)))))
 (def ui-plan (comp/factory Plan {:keyfn :plan/id}))
 (defsc Board [this {:board/keys [id size rows plans] :as props}]
@@ -96,19 +110,29 @@
    :ident :board/id
    :initial-state {:board/id :param/id
                    :board/size :param/size
-                   :board/rows [{:id 4
+                   :board/rows [{:id 7
                                  :number 4
                                  :type :row-type/goal}
+                                {:id 6
+                                 :number 3
+                                 :type :row-type/space}
+                                {:id 5
+                                 :number 2
+                                 :type :row-type/space}
+                                {:id 4
+                                 :number 1
+                                 :type :row-type/space}
                                 {:id 3
                                  :number 3
-                                 :type :row-type/spaces}
+                                 :type :row-type/goal}
                                 {:id 2
                                  :number 2
                                  :type :row-type/spaces}
                                 {:id 1
                                  :number 1
                                  :type :row-type/spaces}]
-                   :board/plans []}}
+                   :board/plans [{:id 1
+                                 :number 1}]}}
   (dom/div {:style {:width "100%"}}
     (dom/div {:style {:float "left" :padding "10px"}}
            (dom/h2 {} "Board [" id "] Size " size)
@@ -120,12 +144,26 @@
 (def ui-board (comp/factory Board {:keyfn :board/id}))
 
 (defonce app (-> (app/fulcro-app) (with-react18)))
-
+(def board-rows-size-3 [[:row/id 7]
+                   [:row/id 6]
+                   [:row/id 5]
+                   [:row/id 4]])
+(def board-rows-size-2 [[:row/id 3]
+                        [:row/id 2]
+                        [:row/id 1]])
+(defmutation get-board-of-size-2 [{:board/keys [id]}]
+  (action [{:keys [state]}]
+          (swap! state assoc-in [:board/id 1 :board/size] 2)
+          (swap! state assoc-in [:board/id id :board/rows] board-rows-size-2)))
+(defmutation get-board-of-size-3 [{:board/keys [id]}]
+  (action [{:keys [state]}]
+          (swap! state assoc-in [:board/id 1 :board/size] 3)
+          (swap! state assoc-in [:board/id id :board/rows] board-rows-size-3)))
 (defsc Root [this {:root/keys [board]}]
   {:query [{:root/board (comp/get-query Board)}]
    :initial-state {:root/board {:id 1 :size 0}}}
   (dom/div {} (ui-board board))
-  #_(cond (zero? (:board/size board))
+  (cond (zero? (:board/size board))
         (dom/div {} "select board size: "
                  (dom/button {:onClick #(comp/transact!
                                           this [(get-board-of-size-2 {:board/id (:board/id board)})])
@@ -134,13 +172,11 @@
                                           this [(get-board-of-size-3 {:board/id (:board/id board)})])
                               :style {:margin "0px 15px"}}  " 3 "))
         :else (dom/div {} (ui-board board))))
-
 (defn ^:export init
   "Shadow-cljs sets this up to be our entry-point function. See shadow-cljs.edn `:init-fn` in the modules of the main build."
   []
   (app/mount! app Root "app")
   (js/console.log "Loaded"))
-
 (defn ^:export refresh
   "During development, shadow-cljs will call this on every hot reload of source. See shadow-cljs.edn"
   []
@@ -149,142 +185,7 @@
   ;; As of Fulcro 3.3.0, this addition will help with stale queries when using dynamic routing:
   (comp/refresh-dynamic-queries! app)
   (js/console.log "Hot reload"))
-
-(def test-board {:board/id 1
-                 :board/size 3
-                 :board/plans [{:plan/id 1
-                                :plan/number 1
-                                :plan/steps [{:step/id 1
-                                              :step/number 1
-                                              :step/blocks []
-                                              :step/positions [{:position/id 1
-                                                                :position/row-number 1
-                                                                :position/space-number 2}]}
-                                             {:step/id 2
-                                              :step/number 2
-                                              :step/blocks [{:block/id 1
-                                                             :block/row-number 1
-                                                             :block/space-number 1}]
-                                              :step/positions []}]}]
-                 :board/rows [{:row/id 4
-                               :row/number 4
-                               :row/type :row-type/goal}
-                              {:row/id 3
-                               :row/number 3
-                               :row/type :row-type/spaces
-                               :row/spaces [{:space/id 7
-                                             :space/number 1
-                                             :space/status :free
-                                             :space/occupied []}
-                                            {:space/id 8
-                                             :space/number 2
-                                             :space/status :free
-                                             :space/occupied []}
-                                            {:space/id 9
-                                             :space/number 3
-                                             :space/status :free
-                                             :space/occupied []}]}
-                              {:row/id 2
-                               :row/number 2
-                               :row/type :row-type/spaces
-                               :row/spaces [{:space/id 4
-                                             :space/number 1
-                                             :space/status :free
-                                             :space/occupied []}
-                                            {:space/id 5
-                                             :space/number 2
-                                             :space/status :free
-                                             :space/occupied []}
-                                            {:space/id 6
-                                             :space/number 3
-                                             :space/status :free
-                                             :space/occupied []}]}
-                              {:row/id 1
-                               :row/number 1
-                               :row/type :row-type/spaces
-                               :row/spaces [{:space/id 1
-                                             :space/number 1
-                                             :space/status :blocked
-                                             :space/occupied [{:occupied/id 2
-                                                               :occupied/player :us
-                                                               :occupied/steps [2]}]}
-                                            {:space/id 2
-                                             :space/number 2
-                                             :space/status :occupied
-                                             :space/occupied [{:occupied/id 1
-                                                               :occupied/player :us
-                                                               :occupied/steps [1 2]}]}
-                                            {:space/id 3
-                                             :space/number 3
-                                             :space/status :free
-                                             :space/occupied []}]}]})
-(def new-board {:board/id 1
-                :board/size 3
-                :board/plans [{:plan/id 1
-                               :plan/number 1
-                               :plan/steps [{:step/id 1
-                                             :step/number 1
-                                             :step/blocks []
-                                             :step/positions [{:position/id 1
-                                                               :position/row-number 1
-                                                               :position/space-number 2}]}
-                                            {:step/id 2
-                                             :step/number 2
-                                             :step/blocks [{:block/id 1
-                                                            :block/row-number 1
-                                                            :block/space-number 1}]
-                                             :step/positions []}]}]
-                :board/rows [{:row/id 4
-                              :row/number 4
-                              :row/type :row-type/goal}
-                             {:row/id 3
-                              :row/number 3
-                              :row/type :row-type/spaces
-                              :row/spaces [{:space/id 7
-                                            :space/number 1
-                                            :space/status :free
-                                            :space/occupied []}
-                                           {:space/id 8
-                                            :space/number 2
-                                            :space/status :free
-                                            :space/occupied []}
-                                           {:space/id 9
-                                            :space/number 3
-                                            :space/status :free
-                                            :space/occupied []}]}
-                             {:row/id 2
-                              :row/number 2
-                              :row/type :row-type/spaces
-                              :row/spaces [{:space/id 4
-                                            :space/number 1
-                                            :space/status :free
-                                            :space/occupied []}
-                                           {:space/id 5
-                                            :space/number 2
-                                            :space/status :free
-                                            :space/occupied []}
-                                           {:space/id 6
-                                            :space/number 3
-                                            :space/status :free
-                                            :space/occupied []}]}
-                             {:row/id 1
-                              :row/number 1
-                              :row/type :row-type/spaces
-                              :row/spaces [{:space/id 1
-                                            :space/number 1
-                                            :space/status :free
-                                            :space/occupied []}
-                                           {:space/id 2
-                                            :space/number 2
-                                            :space/status :occupied
-                                            :space/occupied [{:occupied/id 1
-                                                              :occupied/player :us
-                                                              :occupied/steps [1]}]}
-                                           {:space/id 3
-                                            :space/number 3
-                                            :space/status :free
-                                            :space/occupied []}]}]})
 (comment
-  (swap! (::app/state-atom app) assoc-in [:occupied/id 1 :occupied/steps] [1 2])
+  (swap! (::app/state-atom app) assoc-in [:occupied-step/id 1 :occupied-step/steps] [1 2])
   (app/current-state app)
   )
