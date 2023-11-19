7cce6b69a46e0074d72c9b1c9452ee6743eb70ef
Ryan Khetlyr <ryan_khetlyr@waed.circ9.dcn>
2023-11-19 13:42:42 -0800

    show board state buttons only on adjascent spaces

--- src/main/app/ui.cljs
+++ src/main/app/ui.cljs
@@ -23,62 +23,90 @@
 (defmutation make-occupied [{:keys [space/id]}]
              (action [{:keys [state]}]
                      (swap! state assoc-in [:space/id id :space/occupant] :player)
-                     (let [active-board-id (get-in @state [:state-data/id :board :state-data/active-board-id])
-                           old-steps (get-in @state [:space/id id :space/occupied-steps])
-                           new-step-number (inc (get-in @state [:board/id active-board-id :board/step-number]))
-                           new-steps (conj old-steps new-step-number)]
-                       (swap! state assoc-in [:state-data/id :board :state-data/player-space-id] id)
-                       (swap! state assoc-in [:space/id id :space/occupied-steps] new-steps)
-                       (swap! state assoc-in [:board/id active-board-id :board/step-number] new-step-number))))
+                     (let [active-board-id (get-in @state [:state-data/id :board :state-data/active-board-id])]
+                         (let [old-steps (get-in @state [:space/id id :space/occupied-steps])
+                               new-step-number (inc (get-in @state [:board/id active-board-id :board/step-number]))
+                               new-steps (conj old-steps new-step-number)]
+                           (swap! state assoc-in [:state-data/id :board :state-data/player-space-id] id)
+                           (swap! state assoc-in [:space/id id :space/occupied-steps] new-steps)
+                           (swap! state assoc-in [:board/id active-board-id :board/step-number] new-step-number))
+                         ; reset show-move-block-button for each space
+                         (let [rows (get-in @state [:board/id active-board-id :board/rows])]
+                           (print (str " rows " rows))
+                           #_(map #(let [row %] (do-something row) ) rows)
+                           #_(map (fn [row]
+                                  (print (str " row " row))
+                                  (let [row-spaces (get-in @state (conj row :row/spaces))
+                                        row-number (get-in @state (conj row :row/number))]
+                                    (vec (map (fn [row-space]
+                                                (let [space-number (get-in @state (conj row-space :space/number))]
+                                                  (print (str " row space " row-space
+                                                              " row number" row-number
+                                                              " space number " space-number))
+                                                  (swap! state assoc-in (conj row-space :space/show-move-block-button) true )))
+                                              row-spaces)))) rows)))))
 (defn space-css [type occupant]
   {:className (str "space "
                    (if (= occupant :player) "occupied ")
                    (if (= occupant :blocker) "blocked ")
                    (if (= type :row-type/goal) "goal "))})
 
-(defsc Space [this {:space/keys [id number occupant occupied-steps]} {:keys [onSpaceStep]}]
-       {:query [:space/id :space/number :space/occupant :space/occupied-steps]
+(defsc Space [this {:space/keys [id number occupant occupied-steps show-move-block-button]}]
+       {:query [:space/id :space/number :space/occupant :space/occupied-steps :space/show-move-block-button]
         :ident :space/id
-        :initial-state (fn [{:keys [id number occupant]}]
+        :initial-state (fn [{:keys [id number occupant show-move-block-button]}]
                          {:space/id id
+                          :space/show-move-block-button show-move-block-button
                           :space/number number
                           :space/occupant occupant
                           :space/occupied-steps (cond (= occupant :player) [1]
                                                       :else [])})}
-       (dom/span (space-css nil occupant) "Space " id " steps " (str occupied-steps " " occupant)
-                 (dom/button {:onClick #(comp/transact! this [(make-occupied {:space/id id})]) :style {:margin "0px 15px"}}
-                             " move ")
-                 (dom/button {:onClick #(comp/transact! this [(make-blocked {:space/id id})]) :style {:margin "0px 15px"}}
-                             " block ")))
+       (dom/span (space-css nil occupant)
+                 (if (true? show-move-block-button) (dom/button {:onClick #(comp/transact! this [(make-occupied {:space/id id})]) :style {:margin "0px 15px"}}
+                             " move "))
+                 (if (true? show-move-block-button) (dom/button {:onClick #(comp/transact! this [(make-blocked {:space/id id})]) :style {:margin "0px 15px"}}
+                             " block "))))
 (def ui-space (comp/factory Space {:keyfn :space/id}))
 
 (defsc Row [this {:row/keys [id number type spaces] :as props}]
-       {:query [:row/id :row/number :row/type {:row/spaces (comp/get-query Space)}]
-        :ident :row/id
-        :initial-state (fn [{:keys [id number type]}]
-                         {:row/id id
-                          :row/number number
-                          :row/type type
-                          :row/spaces (cond (> id 3) [(comp/get-initial-state Space {:id (+ (* (- number 1) 3) 1)
-                                                                                     :number 1
-                                                                                     :occupant nil})
-                                                      (comp/get-initial-state Space {:id (+ (* (- number 1) 3) 2)
-                                                                                     :number 2
-                                                                                     :occupant (cond (= number 1) :player
-                                                                                                   :else nil)})
-                                                      (comp/get-initial-state Space {:id (+ (* (- number 1) 3) 3)
-                                                                                     :number 3
-                                                                                     :occupant nil})]
-                                            :else [(comp/get-initial-state Space {:id (+ (* (- number 1) 3) 1)
-                                                                                  :number 1
-                                                                                  :occupant nil})
-                                                   (comp/get-initial-state Space {:id (+ (* (- number 1) 3) 2)
-                                                                                  :number 2
-                                                                                  :occupant (cond (= number 1) :player
-                                                                                                :else nil)})])})}
+  {:query         [:row/id :row/number :row/type {:row/spaces (comp/get-query Space)}]
+   :ident         :row/id
+   :initial-state (fn [{:keys [id number type]}]
+                    {:row/id     id
+                     :row/number number
+                     :row/type   type
+                     :row/spaces (let [row-number number] (cond (> id 3) [(comp/get-initial-state Space {
+                                                                                :id             (+ (* (- row-number 1) 3) 1)
+                                                                                :number         1
+                                                                                :occupant       nil
+                                                                                :show-move-block-button (cond (= row-number 1) true
+                                                                                                              :else false)
+                                                                                })
+                                                 (comp/get-initial-state Space {:id       (+ (* (- row-number 1) 3) 2)
+                                                                                :number   2
+                                                                                :show-move-block-button (cond (= row-number 2) true
+                                                                                                              :else false)
+                                                                                :occupant (cond (= row-number 1) :player
+                                                                                                :else nil)})
+                                                 (comp/get-initial-state Space {:id       (+ (* (- row-number 1) 3) 3)
+                                                                                :number   3
+                                                                                :show-move-block-button (cond (= row-number 1) true
+                                                                                                              :else false)
+                                                                                :occupant nil})]
+                                       :else [(comp/get-initial-state Space {:id       (+ (* (- row-number 1) 3) 1)
+                                                                             :number   1
+                                                                             :show-move-block-button (cond (= row-number 1) true
+                                                                                                           :else false)
+                                                                             :occupant nil})
+                                              (comp/get-initial-state Space {:id       (+ (* (- row-number 1) 3) 2)
+                                                                             :number   2
+                                                                             :show-move-block-button (cond (= row-number 2) true
+                                                                                                           :else false)
+                                                                             :occupant (cond (= row-number 1) :player
+                                                                                             :else nil)})]))})}
        (dom/div {} (dom/div {:style {:padding "5px"}} (map ui-space spaces))))
-
 (def ui-row (comp/factory Row {:keyfn :row/id}))
+
 (defsc Board [this {:board/keys [id size rows step-number] :as props}]
        {:query [:board/id :board/size :board/step-number {:board/rows (comp/get-query Row)}]
         :ident :board/id
@@ -110,7 +138,6 @@
                 (dom/div {:style {:float "left" :padding "10px"}}
                          (dom/h2 {} "Board [" id "] Size " size)
                          (dom/div {} (map ui-row rows)))))
-
 (def ui-board (comp/factory Board {:keyfn :board/id}))
 
 (def board-rows-size-3 [[:row/id 7]
@@ -122,14 +149,17 @@
                         [:row/id 1]])
 (defmutation get-board-of-size-2 [{:board/keys [id]}]
              (action [{:keys [state]}]
+                     (swap! state assoc-in [:state-data/id :board :state-data/size] 2)
                      (swap! state assoc-in [:board/id 1 :board/size] 2)
                      (swap! state assoc-in [:board/id id :board/rows] board-rows-size-2)
                      (swap! state assoc-in [:state-data/id :board :state-data/state] :planning)))
 (defmutation get-board-of-size-3 [{:board/keys [id]}]
              (action [{:keys [state]}]
+                     (swap! state assoc-in [:state-data/id :board :state-data/size] 3)
                      (swap! state assoc-in [:board/id 1 :board/size] 3)
                      (swap! state assoc-in [:board/id id :board/rows] board-rows-size-3)
                      (swap! state assoc-in [:state-data/id :board :state-data/state] :planning)))
+
 (defsc StateData [this {:state-data/keys [id active-board-id player-space-id size state] :as props}]
   {:query [:state-data/id :state-data/active-board-id :state-data/player-space-id :state-data/size :state-data/state]
    :ident :state-data/id
@@ -140,18 +170,25 @@
                    :state-data/state :param/state}}
   (dom/div {} (dom/p {} "state data " (str id " size " size " state " state " active board id " active-board-id " current player space " player-space-id)) ))
 (def ui-state-data (comp/factory StateData {:keyfn :state-data/id}))
+
 (defsc Root [this {:root/keys [board state-data]}]
        {:query [{:root/board (comp/get-query Board)} {:root/state-data (comp/get-query StateData)}]
         :initial-state {:root/board {:id 1 :size 0 :step-number 1}
                         :root/state-data {:id :board :size 0 :active-board-id 1 :player-space-id 2 :state :choose-size}}}
   (dom/div {}
            (ui-state-data state-data)
-           (cond (= (:state-data/state state-data) :choose-size)
+           (let [get-new-board-button (fn [{:keys [size label]}]
+                                        (print "get new board size " size " label " label)
+                                        (dom/button {:onClick #(comp/transact!
+                                                             this [(cond (= size 2) (get-board-of-size-2 {:board/id (:board/id board)})
+                                                                         (= size 3) (get-board-of-size-3 {:board/id (:board/id board)}))])
+                                                 :style {:margin "0px 15px"}}
+                                                                   (str " " (cond (nil? label) size
+                                                                                  :else label) " ")))]
+             (cond (= (:state-data/state state-data) :choose-size)
                  (dom/div {} "select board size: "
-                          (dom/button {:onClick #(comp/transact!
-                                                   this [(get-board-of-size-2 {:board/id (:board/id board)})])
-                                       :style {:margin "0px 15px"}}  " 2 ")
-                          (dom/button {:onClick #(comp/transact!
-                                                   this [(get-board-of-size-3 {:board/id (:board/id board)})])
-                                       :style {:margin "0px 15px"}}  " 3 "))
-                 :else (ui-board board))))
+                          (get-new-board-button {:size 2})
+                          (get-new-board-button {:size 3}))
+                 :else (dom/div
+                         (get-new-board-button {:size (:state-data/size state-data) :label "clear board"})
+                         (ui-board board))))))
